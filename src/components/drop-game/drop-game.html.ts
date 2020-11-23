fab6617c3aeda5d4e21009ef054e64426efbb00c
Keith Hill <keith@dexs.ca>
2020-11-23 09:15:23 -0500

    winterized :)

--- src/components/drop-game/drop-game.html.ts
+++ src/components/drop-game/drop-game.html.ts
@@ -3,7 +3,8 @@ import { html } from 'lit-html';
 export const content = (element: any) => {
     
     return html`
-        <img class="target" src="images/grass-target.png" />
+        <img class="target" src="images/snow-target.png" />
+        <img class="target2" src="images/snow-target.png" />
     `;
 
 };
